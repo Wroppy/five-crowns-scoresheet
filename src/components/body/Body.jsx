87d789a3c98daa91e471a37894645ddf79e50aa9
Weyman Wong <wxwong806@gmail.com>
2024-04-13 23:38:41 +1200

    added reset game button functionality

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -6,11 +6,11 @@ import GamePage from "../game-page/GamePage";
 const Body = () => {
   const [page, setPage] = useState(1);
   const [players, setPlayers] = useState([]); // [[player1, player2, player3, player4
-
+  
   return (
     <div className="body">
       {page === 1 && <PreGamePage setPage={setPage} players={players} setPlayers={setPlayers} />}
-      {page === 2 && <GamePage players={players} setPage={setPage}/>}
+      {page === 2 && <GamePage setPlayers={setPlayers} players={players} setPage={setPage}/>}
     </div>
   );
 };
--- src/components/game-page/GamePage.jsx
+++ src/components/game-page/GamePage.jsx
@@ -3,14 +3,21 @@ import ScoreSheet from "../score-sheet/ScoreSheet";
 import "./GamePage.css";
 import RefreshIcon from '@mui/icons-material/Refresh';
 
-const GamePage = ({ players, setPage }) => {
+const GamePage = ({setPlayers, players, setPage }) => {
+  // Resets the game
+  const resetGame = () => {
+    // Adds a new player and deletes it to update the state
+    setPlayers([ ...players, "player" ]);
+    setPlayers(players.slice(0, players.length - 1));
+  };
+
   return (
     <div className="game-page">
       <ScoreSheet players={players} />
 
       <div className="game-page-footer">
-        <IconButton>
-          <Icon><RefreshIcon/></Icon>
+        <IconButton onClick={resetGame}>
+          <RefreshIcon/>
         </IconButton>
         <Button variant="outlined" onClick={() => setPage(1)}>
           Go Back
