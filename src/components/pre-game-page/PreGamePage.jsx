c250d829b9d7748eb0528994c8d2e103ee0ee4a3
Weyman Wong <wxwong806@gmail.com>
2024-04-13 22:25:43 +1200

    Added players state to the page and linked up form to add players names to the state

--- src/components/pre-game-page/PreGamePage.jsx
+++ src/components/pre-game-page/PreGamePage.jsx
@@ -1,22 +1,38 @@
 import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import "./PreGamePage.css";
+import { useState } from "react";
 
 const PreGamePage = () => {
-  
+  const [players, setPlayers] = useState([]);
+  const [playerName, setPlayerName] = useState("");
+
+  const addPlayer = (e) => {
+    e.preventDefault();
+    // Checks if the player name is empty after trimming the spaces
+    if (!playerName.trim()) {
+      setPlayerName(playerName.trim());
+      return;
+    }
+    setPlayers([...players, playerName]);
+    setPlayerName("");
+  };
+
   return (
     <div className="pre-game-page">
-      <div className="pre-game-page-heading">
-        <TextField
-          type="text"
-          placeholder="Add player name"
-          label="Add player name"
-          variant="outlined"
-          size="small"
-          className="add-player-field"
-        />
-        <Button variant="outlined">Add Player</Button>
-      </div>
+      <form className="pre-game-page-heading" onSubmit={addPlayer}>
+          <TextField
+            type="text"
+            placeholder="Add player name"
+            label="Add player name"
+            variant="outlined"
+            size="small"
+            className="add-player-field"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
+          <Button variant="outlined" type="submit">Add Player</Button>
+      </form>
       <div className="pre-game-page-body">
         <div className="player-list">
           <h2>Players</h2>
@@ -28,7 +44,9 @@ const PreGamePage = () => {
         </div>
       </div>
       <div className="pre-game-page-footer">
-        <Button className="start-game-button" variant="outlined">Start Game</Button>
+        <Button className="start-game-button" variant="outlined">
+          Start Game
+        </Button>
       </div>
     </div>
   );
