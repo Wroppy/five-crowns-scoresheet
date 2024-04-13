4f1d2df64c4ada652a3c44f3bbe3de80a2788cfd
Weyman Wong <wxwong806@gmail.com>
2024-04-13 22:18:45 +1200

    Added ui for the player list

--- src/components/pre-game-page/PreGamePage.jsx
+++ src/components/pre-game-page/PreGamePage.jsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 import "./PreGamePage.css";
 
 const PreGamePage = () => {
+  
   return (
     <div className="pre-game-page">
       <div className="pre-game-page-heading">
@@ -16,7 +17,16 @@ const PreGamePage = () => {
         />
         <Button variant="outlined">Add Player</Button>
       </div>
-      <div className="pre-game-page-body"></div>
+      <div className="pre-game-page-body">
+        <div className="player-list">
+          <h2>Players</h2>
+          <ul>
+            <li>Player 1</li>
+            <li>Player 2</li>
+            <li>Player 3</li>
+          </ul>
+        </div>
+      </div>
       <div className="pre-game-page-footer">
         <Button className="start-game-button" variant="outlined">Start Game</Button>
       </div>
